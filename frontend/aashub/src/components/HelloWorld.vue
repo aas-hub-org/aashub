28c690009ee000496bb62e3e5b6d91dc2db480cb
Aaron Zielstorff <aaron.zi@web.de>
2024-08-07 20:03:51 +0000

    Adds tasks to launch frontend

--- frontend/aashub/src/components/HelloWorld.vue
+++ frontend/aashub/src/components/HelloWorld.vue
@@ -11,9 +11,9 @@
       />
 
       <div class="text-center">
-        <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+        <div class="text-body-2 font-weight-light mb-n1">Welcome to the</div>
 
-        <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+        <h1 class="text-h2 font-weight-bold">AAS Hub</h1>
       </div>
 
       <div class="py-4" />
--- frontend/aashub/vite.config.mts
+++ frontend/aashub/vite.config.mts
@@ -42,6 +42,15 @@ export default defineConfig({
     ],
   },
   server: {
+    host: '0.0.0.0',
     port: 3000,
-  },
+    watch: {
+      usePolling: true
+    },
+    hmr: {
+      host: 'host.docker.internal', // Ensures HMR tries to connect to localhost
+      port: 3000,
+      protocol: 'ws'
+    }
+  }
 })
